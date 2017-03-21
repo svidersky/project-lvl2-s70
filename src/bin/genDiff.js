38a934553f3307c89405eb4af20eab72af86a403
Leonid Sviderskii <svidersky.leonid@gmail.com>
2017-03-21 09:53:26 -0400

    Replaced .usage

--- src/bin/genDiff.js
+++ src/bin/genDiff.js
@@ -3,8 +3,8 @@
 const program = require('commander');
 
 program
-  .version('0.0.1')
-  .usage('[options] <first_config> <second_config>')
+  .version('1.0.3')
+  .arguments('<first_config> <second_config>')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format')
   .parse(process.argv);
